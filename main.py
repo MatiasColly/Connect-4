649c5080e6d1dcab79c2416961d6b35148cbe373
Matias Colly <matiascolly@gmail.com>
2023-01-20 14:05:35 -0300

    1.1.3: Added WIN/DRAW message in screen. Added functioning comments

--- main.py
+++ main.py
@@ -4,29 +4,31 @@ import sys
 from boardClass import Board
 from minimax import analyze_best_move
 
-# Game Parameter
+# Game Parameters
 currentPlayer = 1
+MAX_ROW = 6
+MAX_COLUMN = 7
 
 # AI Parameters
 AUTO_PLAY_AI = False
 AI_PLAY_FIRST = False
 
-# Pygame parameters
-MAX_ROW = 6
-MAX_COLUMN = 7
-
+# RGB Colours
 BLUE_COLOUR = (0, 0, 255)
 BLACK_COLOUR = (0, 0, 0)
 RED_COLOUR = (255, 0, 0)
 YELLOW_COLOUR = (255, 255, 0)
 WHITE_COLOUR = (255, 255, 255)
 
+# Pygame board parameters
 SQUARE_SIZE = 100
 GRAPH_WIDTH = MAX_COLUMN * SQUARE_SIZE
 GRAPH_HEIGHT = MAX_ROW * SQUARE_SIZE
 MENU_WIDTH = 200
 GRAPH_SIZE = (GRAPH_WIDTH + MENU_WIDTH, GRAPH_HEIGHT)
 CURRENT_TURN_DIS_POS = (GRAPH_WIDTH + 100, 90)
+
+
 def main():
     # Pygame and board class init
     pygame.init()
@@ -34,67 +36,81 @@ def main():
     screen = pygame.display.set_mode(GRAPH_SIZE)
     draw_board(screen)
     draw_menu_text(screen)
-    # Board object
+
+    # Board object. All game mechanics are coded in Board class
     board = Board()
 
     returnValue = "NO WIN"
     global currentPlayer
-    inputValue = 0
     global AUTO_PLAY_AI
+    ai_to_play = AI_PLAY_FIRST
+    best_move = analyze_best_move(board, currentPlayer)
 
-    if AI_PLAY_FIRST:
-        ai_to_play = True
-        best_move = analyze_best_move(board, currentPlayer)
-    else:
-        ai_to_play = False
-
+    # Main pygame loop
     while returnValue != "WIN" and returnValue != "DRAW":
         for event in pygame.event.get():
 
+            # Top right corner 'X' for game quitting
             if event.type == pygame.QUIT:
                 sys.exit()
 
+            # Mouse button press defines where the piece is being played
             if event.type == pygame.MOUSEBUTTONDOWN or ai_to_play:
                 if ai_to_play:
                     inputValue = best_move
                 else:
                     inputValue = int(event.pos[0] / 100)
 
+                # Update object with the new piece placement
                 returnValue = board.drop_piece(inputValue, currentPlayer)
 
+                # Return "INVALID" if column is full or mouse button press is out of game's boundaries
                 if returnValue == "INVALID":
                     print("Invalid input, insert again")
+
                 else:
+                    # Log debbuging print
                     board.print_board()
+
+                    # Pygame's piece add to game screen
                     add_piece(MAX_ROW - board.column_height(inputValue), inputValue, currentPlayer, screen)
-                    if returnValue == "WIN":
-                        print("Player", currentPlayer, "WON!")
-                    if returnValue == "DRAW":
-                        print("Game is a DRAW")
+
+                    if returnValue == "WIN" or returnValue == "DRAW":
+                        draw_game_end(screen, returnValue)
+
+                    # Switch turn to other player and analyze it's best move
                     currentPlayer = 3 - currentPlayer
                     best_move = analyze_best_move(board, currentPlayer)
+
+                    # Toggle AI's flag to play next turn if autoplay is enabled
                     if AUTO_PLAY_AI:
                         ai_to_play = not ai_to_play
                     else:
                         ai_to_play = False
 
+            # Hotkeys menu
             if event.type == pygame.KEYDOWN:
+
+                # a: toggles AI's autoplay
                 if event.key == pygame.K_a:
                     AUTO_PLAY_AI = not AUTO_PLAY_AI
                     draw_menu_text(screen)
                     print("Auto play AI:", AUTO_PLAY_AI)
+
+                # s: play's best move according to AI
                 if event.key == pygame.K_s:
                     print("Playing best move")
                     ai_to_play = True
 
-
-
+    # After game is finished, mouse button press is needed to avoid pygame quitting without letting the player see
+    # the final board
     while 1:
         for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.QUIT:
                 sys.exit()
 
 
+# Draw Connect 4 grid
 def draw_board(screen):
     pygame.draw.rect(screen, BLUE_COLOUR, (0, 0, GRAPH_WIDTH, GRAPH_HEIGHT))
     for row in range(MAX_ROW):
@@ -103,6 +119,8 @@ def draw_board(screen):
                                 (row * SQUARE_SIZE) + SQUARE_SIZE / 2), radius=SQUARE_SIZE * 2/5)
     pygame.display.update()
 
+
+# Draw right side menu
 def draw_menu_text(screen):
 
     # Cleans previous menu
@@ -119,7 +137,7 @@ def draw_menu_text(screen):
 
     # AI autoplay box
     textTBD = textfont.render(" AI autoplay:", 1, WHITE_COLOUR)
-    screen.blit(textTBD, (GRAPH_WIDTH + 20, 130))
+    screen.blit(textTBD, (GRAPH_WIDTH + 15, 130))
     if AUTO_PLAY_AI:
         textTBD = textfont.render("ON", 1, WHITE_COLOUR)
         screen.blit(textTBD, (GRAPH_WIDTH + 85, 160))
@@ -139,6 +157,34 @@ def draw_menu_text(screen):
 
     pygame.display.update()
 
+
+# Screen print when game ends
+def draw_game_end(screen, returnValue):
+
+    textfont = pygame.font.SysFont("monospace", 20)
+
+    if returnValue == "WIN":
+        if currentPlayer == 1:
+            textTBD = textfont.render("  Red Player", 1, RED_COLOUR)
+            screen.blit(textTBD, (GRAPH_WIDTH + 20, 200))
+            textTBD = textfont.render("     WON!", 1, RED_COLOUR)
+            screen.blit(textTBD, (GRAPH_WIDTH + 20, 220))
+        else:
+            textTBD = textfont.render(" Yellow Player", 1, YELLOW_COLOUR)
+            screen.blit(textTBD, (GRAPH_WIDTH + 20, 200))
+            textTBD = textfont.render("     WON!", 1, YELLOW_COLOUR)
+            screen.blit(textTBD, (GRAPH_WIDTH + 20, 220))
+
+    if returnValue == "DRAW":
+        textTBD = textfont.render("Game ended in", 1, WHITE_COLOUR)
+        screen.blit(textTBD, (GRAPH_WIDTH + 20, 200))
+        textTBD = textfont.render("     DRAW", 1, WHITE_COLOUR)
+        screen.blit(textTBD, (GRAPH_WIDTH + 20, 220))
+
+    pygame.display.update()
+
+
+# Adds recent played piece in to the grid
 def add_piece(row, column, player, screen):
     if player == 1:
         pygame.draw.circle(screen, RED_COLOUR, center=((column * SQUARE_SIZE) + SQUARE_SIZE / 2,
